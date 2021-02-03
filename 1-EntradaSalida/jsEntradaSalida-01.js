b60737a4492b9bc66b6b570dc77723b91a6bf88e
yamila <yamilakp12@hotmail.com>
2021-02-03 12:01:13 -0300

    init

---
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -0,0 +1,11 @@
+/*
+Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+{function mostrar()
+	
+
+	}
+
+/* asi es como hacemos comentarios con esta modalidad de barras y asteriscos
+*/
+
